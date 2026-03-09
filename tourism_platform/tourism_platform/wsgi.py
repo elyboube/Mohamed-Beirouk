30950a3771b759729390268563168f564e80807a
sidahmed ely <sidahmed@example.com>
2026-03-09 05:00:35 +0000

    Fix: correct settings module in wsgi.py

--- tourism_platform/tourism_platform/wsgi.py
+++ tourism_platform/tourism_platform/wsgi.py
@@ -11,5 +11,5 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tourism_platform.tourism_platform.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tourism_platform.settings')
 application = get_wsgi_application()
